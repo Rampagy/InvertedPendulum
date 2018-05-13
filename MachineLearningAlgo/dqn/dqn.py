1ba88227805cae89b74d2598d2018cfef37bda3e
Rampagy <ajsimon@mtu.edu>
2018-05-13 15:31:32 -0500

    tuned it up a bit and added additional options

--- MachineLearningAlgo/dqn/dqn.py
+++ MachineLearningAlgo/dqn/dqn.py
@@ -4,11 +4,13 @@ import gym
 import numpy as np
 from collections import deque
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.optimizers import Adam
 
-EPISODES = 1000
-TEST = True
+EPISODES = 2000
+TEST = False
+SAVE_LOC = './cartpole-dqn.h5'
+ENV_NAME = 'CartPole-v1'
 
 
 class DQNAgent:
@@ -16,13 +18,13 @@ class DQNAgent:
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
-        self.gamma = 0.95    # discount rate
+        self.gamma = 0.90    # discount rate
         if TEST:
             self.epsilon = 0.0 # exploration rate
         else:
             self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01 # exploration min
-        self.epsilon_decay = 0.995 # exploration decay
+        self.epsilon_decay = 0.996 # exploration decay
         self.learning_rate = 0.001
         self.model = self._build_model()
 
@@ -66,43 +68,46 @@ class DQNAgent:
 
 
 if __name__ == "__main__":
-    env = gym.make('CartPole-v1')
+    env = gym.make(ENV_NAME)
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     agent = DQNAgent(state_size, action_size)
     done = False
+    score = 0
 
     if not TEST:
-        #agent.load("./cartpole-dqn.h5")
+        #agent.load(SAVE_LOC)
         batch_size = 32
 
         for e in range(EPISODES):
+            score = 0
             state = env.reset()
             state = np.reshape(state, [1, state_size])
-            for time in range(500):
+            for _ in range(500):
                 # env.render()
                 action = agent.act(state)
                 next_state, reward, done, _ = env.step(action)
+                score += reward
                 reward = reward if not done else -10
                 next_state = np.reshape(next_state, [1, state_size])
                 agent.remember(state, action, reward, next_state, done)
                 state = next_state
                 if done:
                     print("episode: {}/{}, score: {}, e: {:.2}"
-                        .format(e, EPISODES, time, agent.epsilon))
+                        .format(e, EPISODES, int(score), agent.epsilon))
                     break
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
             if e % 10 == 0:
-                agent.save("./cartpole-dqn.h5")
+                agent.save(SAVE_LOC)
     else:
-        agent.load("./cartpole-dqn.h5")
+        agent.load(SAVE_LOC)
         state = env.reset()
-        max_reward = 0
+
         while not done:
             env.render()
             state = np.reshape(state, [1, state_size])
             action = agent.act(state)
             state, reward, done, _ = env.step(action)
-            max_reward += reward
-        print('score: {}'.format(max_reward))
+            score += reward
+        print('score: {}'.format(score))
